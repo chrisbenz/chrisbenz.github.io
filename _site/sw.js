95d4c6bd1b75bbe25ab856faed73bf240afeca87
chrisbenz <cpbenz@protonmail.com>
2020-08-25 22:31:10 -0700

    Updated about me section

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20200824232638";
+var CACHE_NAME = "saikiransripada-20200825222924";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202008242326",
+        "/css/style.css?202008252229",
         "/"
       ]);
     })
--- about.md
+++ about.md
@@ -5,4 +5,7 @@ permalink: /about/
 tags: about
 ---
 
+Thanks for visiting my site! While I am currently a software developer by trade, 
+I also enjoy learning about a variety of cybersecurity topics, learning new tools for web application 
+pentesting, and pursuing bug bounties.  
 
